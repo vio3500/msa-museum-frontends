da0c8e8676bb89c8b062eea73f2fc23d747271bc
vio3500 <vio3500@github>
2024-11-28 23:46:22 +0800

    Upload initial webpage

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="moon-container">
+          <div className="moon"></div>
+      </div>
+        <div className="content">
+        </div>
+    </div>
+  );
+}
+
+export default App;
